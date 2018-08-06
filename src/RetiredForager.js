1bc477b1dd11d7df7f89a118740c8cf6ebdac88c
shabbyjoon <ShabnamMokhtarani@Shabnams-MacBook-Pro.local>
2018-08-06 11:52:15 -0700

    implements RetiredForager.js pseudoclassically

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,23 @@
-var RetiredForagerBee = function() {
+var RetiredForagerBee = function(age, color, food, job, canFly, treasureChest) {
+	ForagerBee.call(this, age, color, food, job, canFly, treasureChest);
+	this.age = 40;
+	this.job = 'gamble';
+	this.canFly = false;
+	this.color = 'grey';
+
+
 };
+
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+RetiredForagerBee.prototype.forage = function() {
+	return 'I am too old, let me play cards instead';
+
+}
+
+RetiredForagerBee.prototype.gamble = function() {
+	this.treasureChest.push('treasure');
+
+}
 
