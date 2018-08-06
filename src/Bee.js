bf4588eaffc0f162c6046e212db9af3683703712
shabbyjoon <ShabnamMokhtarani@Shabnams-MacBook-Pro.local>
2018-08-06 11:09:01 -0700

    implements Bee.js pseudoclassically with passing tests

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,10 @@
-var Bee = function() {
+var Bee = function(age, color, food) {
+	Grub.call(this, age, color, food);
+	this.age = 5;
+	this.color = 'yellow';
+	this.job = 'keep on growing';
 };
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
 
