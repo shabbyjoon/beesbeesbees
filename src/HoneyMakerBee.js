649cc506538706d6f57eaf53c39fabdfbc11aa8b
shabbyjoon <ShabnamMokhtarani@Shabnams-MacBook-Pro.local>
2018-08-06 11:38:19 -0700

    implements HoneyMakerBee.js pseudoclassically with passing tests

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,17 @@
-var HoneyMakerBee = function() {
+var HoneyMakerBee = function(age, color, food, job) {
+	Bee.call(this, age, color, food, job);
+	this.age = 10;
+	this.job = 'make honey';
+	this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot = this.honeyPot + 1;	 
+}
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot = this.honeyPot - 1;
+}
