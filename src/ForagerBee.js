2df37acf3a0cc82f0e05d7ae3f6fa3b89e2a2e6b
shabbyjoon <ShabnamMokhtarani@Shabnams-MacBook-Pro.local>
2018-08-06 11:43:11 -0700

    implements ForagerBee.js pseudoclassically with passing tests

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,15 @@
-var ForagerBee = function() {
+var ForagerBee = function(age, color, food, job) {
+  HoneyMakerBee.call(this, age, color, food, job);
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
 };
+
+ForagerBee.prototype = Object.create(HoneyMakerBee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.forage = function() {
+	this.treasureChest.push('treasure');
+}
+
 
